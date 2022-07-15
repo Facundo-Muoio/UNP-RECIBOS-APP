8bf1ce5c45502a5cb935efcf6e82affb664777df
Facundo Muoio <facundo.muoio@gmail.com>
2022-07-15 12:37:37 -0300

    Remove ignored files

--- src/public/js/descargar.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', (e) => {
-    const divDownload  = document.querySelector("#divDownload")
-    
-    setTimeout(() => {
-        divDownload.innerHTML = `
-        <a href="../pdf/recibos.pdf" download="recibos.pdf"><button class="btn btn-outline-danger">DESCARGAR PDF</button></a>
-        `
-    },1500)    
-});
--- src/routes/Home.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express")
-const Home = Router()
-
-Home.get("/", (req, res) => {
-    res.render("home")
-})
-
-module.exports = Home

5b4ff646d1f70a3ac619ad6f8dc1f9a38daa4eb7
Facundo Muoio <facundo.muoio@gmail.com>
2022-11-28 19:53:44 -0300

    added pdf and routes

--- src/routes/Resumen.js
+++ src/routes/Resumen.js
@@ -8,14 +8,16 @@ Resumen.get("/resumen", async (req, res) => {
     const empleados = await Employee.find({})
     const extras = await Extra.find({})
     const fecha = await Fecha.find({})
-    let sueldo = 0
-    empleados.forEach(e => {
-        sueldo += e.salary + Number(e.extras)
-    })
-    extras.forEach(e => {
-       sueldo += e.salary
-    })
-    res.render("Resumen", {empleados, extras, fecha, sueldo})
+    if(empleados || extras){
+        let sueldo = 0
+        empleados.forEach(e => {
+            sueldo += e.salary + Number(e.extras)
+        })
+        extras.forEach(e => {
+           sueldo += e.salary
+        })
+        res.render("Resumen", {empleados, extras, fecha, sueldo})
+    }
 })
 
 module.exports = Resumen

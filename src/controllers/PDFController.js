561cfca6379f52ada8cd4f4db4f641cf13fce3ad
Facundo Muoio <facundo.muoio@gmail.com>
2022-11-29 20:06:24 -0300

    changing html-pdf for puppeteer

---
+++ src/controllers/PDFController.js
@@ -0,0 +1,26 @@
+const puppeteer = require("puppeteer");
+
+async function createRecibos(html) {
+  //abrimos el navegador
+  let navegador = await puppeteer.launch();
+  let page = await navegador.newPage();
+
+  await page.setContent(html);
+  // await page.emulateMedia("screen");
+  let pdf = await page.pdf({
+    path: "src/public/pdf/recibos.pdf",
+    format: "A4",
+    margin: {
+      top: "2px",
+      left: "40px",
+    },
+    printBackground: true,
+  });
+
+  //cerramos el navegador
+  await navegador.close();
+
+  return pdf;
+}
+
+module.exports = { createRecibos };
--- src/routes/Descargar.js
+++ src/routes/Descargar.js
@@ -1,30 +1,34 @@
-const { Router } = require("express")
-const Descargar = Router()
-const path = require("path")
-const pdf = require("html-pdf")
-const fs = require("fs")
-const os = require("os")
-const { generateHTML } = require("../controllers/traerHtml")
-let options = { 
-    format: 'A4',
-    border: {
-        top: "2px",
-        rigth:`${os.platform() !== "win32" ? "70px" : "60px"}`,
-        left: `${os.platform() !== "win32" ? "30px" : "40px"}`
-    }, 
-};
+const { Router } = require("express");
+const Descargar = Router();
+const path = require("path");
+// const pdf = require("html-pdf");
+const fs = require("fs");
+const os = require("os");
+const { generateHTML } = require("../controllers/traerHtml");
+const { createRecibos } = require("../controllers/PDFController");
 
+// let options = {
+//     format: 'A4',
+//     border: {
+//         top: "2px",
+//         rigth:`${os.platform() !== "win32" ? "70px" : "60px"}`,
+//         left: `${os.platform() !== "win32" ? "30px" : "40px"}`
+//     },
+// };
 
 Descargar.get("/descargar", async (req, res) => {
-    const html = await generateHTML() 
-    fs.unlink(`${path.join(__dirname, "../", "public", "pdf", "recibos.pdf")}`, (error) => {
-        if(error) throw error;
-    })   
-    pdf.create(html, options).toFile(path.join(__dirname,"../","public","pdf","recibos.pdf"), function(err, res) {
-        if (err) return console.log(err);
-        console.log(res);
-    });
-    res.render("Descargar")
-})
+  const html = await generateHTML();
+  await createRecibos(html);
+  res.render("Descargar");
 
-module.exports = Descargar
+  // fs.unlink(`${path.join(__dirname, "../", "public", "pdf", "recibos.pdf")}`, (error) => {
+  //     if(error) throw error;
+  // })
+  // pdf.create(html, options).toFile(path.join(__dirname,"../","public","pdf","recibos.pdf"), function(err, res) {
+  //     if (err) return console.log(err);
+  //     console.log(res);
+  // });
+  // res.render("Descargar")
+});
+
+module.exports = Descargar;
